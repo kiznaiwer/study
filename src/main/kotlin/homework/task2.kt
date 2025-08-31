16db156c4d7d83b32f1b91edb226f55084b8641b
Алексей <1999ctalih@gmail.com>
2025-08-31 22:45:52 +0300

    rename task1

--- src/main/kotlin/homework/task2.kt
+++ src/main/kotlin/homework/task2.kt
@@ -1,2 +1,17 @@
 package homework
 
+ fun main(){
+     print("Введите количество баллов: ")
+     val scores = readln()?.toIntOrNull()
+
+     val certification = when (scores){
+         in 90..100 ->"A"
+         in 75..89 ->"B"
+         in 50..74 -> "C"
+         in 30..49 ->"D"
+         in 0..29 ->"F"
+         else -> {"Ошибка"}
+     }
+
+     println("Оценка студента: $certification")
+ }
